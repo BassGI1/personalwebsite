f40ca29f362955e57de3d9e16452f5dd9cca946f
Bassam <basmaym148@gmail.com>
2022-05-07 19:27:47 -0400

    Added latest video

--- src/components/pages/Home/Vid.js
+++ src/components/pages/Home/Vid.js
@@ -8,7 +8,7 @@ export default function Vid(props) {
 
             <h3 style={{margin: "5% auto 5% 45%"}}>{props.term}</h3>
 
-            <iframe width="668" height="375.75" src={props.src} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen style={{borderRadius: "2.5vw"}}></iframe>
+            <iframe width="800" height="450" src={props.src} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen style={{borderRadius: "2.5vw"}}></iframe>
 
         </div>
 
