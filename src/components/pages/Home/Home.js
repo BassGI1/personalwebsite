a57de75ba2cb3b013a0781ccc846aeb91111c4fc
Bassam El-Naggar <89227338+BassGI1@users.noreply.github.com>
2022-11-17 20:44:01 -0500

    Update Home.js

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -10,7 +10,7 @@ export default function Home(props) {
             
             <h1 style={{color: colour, fontSize: "3.25rem"}} className="welcome">Welcome to my personal website!</h1>
 
-            <p style={{color: colour}} className="intro">If you're here, it means you've probably received an application from me. This website acts as an extended resume which allows me to 1. show off my web development skills, 2. convince you to hire me (hopefully!) , and 3. have fun showing off my projects! Feel free to have a look around and check out all of the pages! Below you'll find my most recent co-op video, underneath will be a way to access all of my previous co-op videos, in case you want to see what I was like in the past.</p>
+{/*<p style={{color: colour}} className="intro">If you're here, it means you've probably received an application from me. This website acts as an extended resume which allows me to 1. show off my web development skills, 2. convince you to hire me (hopefully!) , and 3. have fun showing off my projects! Feel free to have a look around and check out all of the pages! Below you'll find my most recent co-op video, underneath will be a way to access all of my previous co-op videos, in case you want to see what I was like in the past.</p>*/}
 
             <iframe width="600" height="350" src="https://www.youtube.com/embed/ewkrrM4R33A" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen className="video"></iframe>
 
