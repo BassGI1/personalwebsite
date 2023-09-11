eb04b23ab48974f251caf0c463f7e8ffa7793ab0
Bassam <basmaym148@gmail.com>
2023-09-11 18:12:22 -0400

    updated harmonise

--- src/components/pages/Projects/Project.js
+++ src/components/pages/Projects/Project.js
@@ -10,7 +10,7 @@ export default function Project(props) {
             <span style={{gridColumnStart: "1", margin: "2vh 3vw 0px 3vw"}}>{props.codedin.map(x => <img alt="pic" src={x} style={{height: "5vh", marginRight: "2vw"}} key={x}/>)}</span>
             <h4 style={{gridColumnStart: "1", margin: "2vh 3vw 0px 3vw"}}>{props.description}</h4>
             <a href={props.link} style={{gridColumnStart: "1", margin: "2vh 3vw 0px 3vw", cursor: props.link === '#' ? "default" : "pointer"}}>{props.link === '#' ? '' : props.link}</a>
-            <img src={process.env.PUBLIC_URL + `/projectImages/${props.src}.png`} alt="pic" style={{display: "flex", height: "40vh", margin: "auto auto 3vh auto", border: "solid 2.5px #003ec5", borderRadius: "3vh"}}/>
+            <img src={process.env.PUBLIC_URL + `/projectImages/${props.src}`} alt="pic" style={{display: "flex", height: "40vh", margin: "auto auto 3vh auto", border: "solid 2.5px #003ec5", borderRadius: "3vh"}}/>
             
         </div>
 
--- src/components/pages/Projects/ProjectsData.js
+++ src/components/pages/Projects/ProjectsData.js
@@ -1,119 +1,119 @@
 const Data = [
     {
         title: "Harmonise",
-        src: "harmonise",
+        src: "harmonise.gif",
         codedin: ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Amazon-S3-Logo.svg/1712px-Amazon-S3-Logo.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/MongoDB_Logo.svg/2560px-MongoDB_Logo.svg.png", "https://cdn-icons-png.flaticon.com/512/5968/5968322.png", "https://vladmrnv.gallerycdn.vsassets.io/extensions/vladmrnv/expresssnippet/0.2.4/1528939685286/Microsoft.VisualStudio.Services.Icons.Default", "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png", "https://reactrouter.com/_brand/react-router-stacked-color.png" , "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"],
         description: "A fully functional social media application. It uses node.js on the backend and react.js for the frontend. For the databases, an AWS S3 bucket is used for image storage, and the user storage is done with mongoDB. Users are able to make posts, comment on their friends posts, direct message their friends/followers, as well as several other key features of a social media. The aspect that distinguishes harmonise from other social media apps is that it doubles as a music sharing app! Users are able to set a 'theme' song that plays when their profile is visited! You can sign up for free!",
         link: "https://harmonise.netlify.app/"
     },
     {
         title: "Flash Cards",
-        src: "flashcards",
+        src: "flashcards.png",
         codedin: ["https://i.postimg.cc/QMDC7RNf/vue.png", "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"],
         description: "This project is very useful for me whenever I'm studying for something. It's a flash card app with a built-in test mode. Since you're here, I might as well give you something. Put the following code into the app to get a special deck made by me: )jS)j*{Gw$!S3}ziNEwVZRDEmvbY\\~\\HbvTnEb\\q\\\"TYYTZ\\~\\HTbUY\\q?>\\WU\\qe~\\xQvYnWEL\\q\\,mw'TZv\\~\\TLYcvb\\q\\@Yw\"TYYTZwVD0'TOOTbwTLUw@wTZwDEESWLOwIEbwAEKY/\\|~>\\WU\\qr~\\xQvYnWEL\\q\\@IwmEQ`bvwPvbv\\~\\TLYcvb\\q\\MEQ`JvwKvvLwbEQnvUwIbEZwZmwRvbYELTDwcvKYWnvwnEwPvbv/\\|~>\\WU\\qt~\\xQvYnWEL\\q\\,mw^SWDDYq\\~\\TLYcvb\\q\\^vTYELvUw^EIncTbvwVLOWLvvb~w{bvTnWJWnm~wNvLTHWnm~wCWDDWLOLvYYwnEwDvTbL~wNvHPLWHTDwYSWDDY~wNWZvwZTLTOvZvLn~w{EERvbTnWJvLvYY~wvnH/\\|~>\\WU\\qy~\\xQvYnWEL\\q\\,mw)EKKWvYq\\~\\TLYcvb\\q\\^REbnY~w^RvLUWLOwnWZvwcWnPwIbWvLUYwTLUwITZWDm~w&vJvDERWLOwTRRYwDWSvwnPWYwELv~w-vTbLWLO~wTZELOYnwEnPvbwnPWLOY\\|~>\\WU\\qu~\\xQvYnWEL\\q\\NPTLSwmEQ\\~\\TLYcvb\\q\\MEQwZTUvwWnwnEwnPvwvLUwEIwnPWYwUvHS/wNPTLSwmEQwIEbwnTSWLOwnPvwnWZvwnEwJWYWnwZmwcvKYWnvwTLUwHEZvwTDDwnPvwcTmwPvbv.\\|<|%",
         link: "https://bassamsflashcards.netlify.app/"
     },
     {
         title: "Notes Application",
-        src: "notes",
+        src: "notes.png",
         codedin: ["https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png", "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" , "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"],
         description: "This was a super fun project that took me about a day to design and code. It turned out great, just as well as I had hoped it would. It can store and draw from local storage to ensure that any saved notes are not lost.",
         link: "https://bassamsnotesapp.netlify.app/"
     },
     {
         title: "Automatic Wallpaper Changer",
-        src: "wallpaper",
+        src: "wallpaper.png",
         codedin: ["https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png"],
         description: "This is a project that was made out of frustration! I had always wanted some sort of application that would give me fresh new wallpapers whenever I needed them, but such an application didn't exist! I decided to make it myself, and I think it turned out great!",
         link: "https://github.com/BassGI1/wallpaperchanger"
     },
     {
         title: "Tic-Tac-Toe",
-        src: "tic",
+        src: "tic.png",
         codedin: ["https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1822px-ISO_C%2B%2B_Logo.svg.png"],
         description: "This project was written entirely in C++. I made it over the course of one afternoon because I was practicing my C++.",
         link: "https://github.com/BassGI1/TicTacToe"
     },
     {
         title: "Abstract Art Generator",
-        src: "abstract",
+        src: "abstract.png",
         codedin: ["https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png", "https://github.com/opencv/opencv/wiki/logo/OpenCV_logo_black.png"],
         description: "This project was really fun to make! It essentially generates a piece of abstract art depending on the parameters given by the user. Below are two sample generations based on two different parameters.",
         link: "https://github.com/BassGI1/abstract_generator"
     },
     {
         title: "Desktop Calculator",
-        src: "csharpcalc",
+        src: "csharpcalc.png",
         codedin: ["https://static-00.iconduck.com/assets.00/c-sharp-c-icon-456x512-9sej0lrz.png", "https://static.thenounproject.com/png/115701-200.png"],
         description: "This project was written using the WPF framework on C#. It is a fully functional desktop calculator",
         link: "https://github.com/BassGI1/C--Calculator"
     },
     {
         title: "Apples to Oranges",
-        src: "applestooranges",
+        src: "applestooranges.png",
         codedin: ["https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png", "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" , "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"],
         description: "This was definitely my most ambitious web app yet, but it turned out great! I tried to make as many animations as possible, so the game looks and feels more aesthetically pleasing. It's fully functional, and (in my opinion) a little bit addictive, so be careful! Can you beat my personal record of 210?",
         link: "https://applestooranges.netlify.app/"
     },
     {
         title: "Personal Website",
-        src: "website",
+        src: "website.png",
         codedin: ["https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png", "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"],
         description: "This is the project that you're currently on. It was made using React.js, and I aim to maintain and update this project for the next few years.",
         link: "#"
     },
     {
         title: "YouTube to MP3",
-        src: "youtubetomp3",
+        src: "youtubetomp3.png",
         codedin: ["https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png"],
         description: "This is a project that was surprisingly simple to code; I made it in about an hour! It's also one that has been quite useful funnily enough.",
         link: "https://github.com/BassGI1/youtubetomp3"
     },
     {
         title: "Pomodoro Timer",
-        src: "pomodoro",
+        src: "pomodoro.png",
         codedin: ["https://i.postimg.cc/QMDC7RNf/vue.png", "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"],
         description: "This is a super useful project that took about a day and a half to design and code. Essentially, a pomodoro timer is an interval system which maximizes efficiency. I use this project all the time; to code, work, study, etc. If you're wondering why there is a very strange fact on the picture, it's because the rest interval cycles through a list of random facts.",
         link: "https://bassamspomodoro.netlify.app/"
     },
     {
         title: "Tracker App",
-        src: "tracker",
+        src: "tracker.png",
         codedin: ["https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png", "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"],
         description: "This is a project that I use on a daily basis. It's an application that tracks . . . anything you want it to! I use it to track my sleep quality everyday. It is also able to keep a record of previous months. In the top right, it gives a cumulative 'score' of the historical ratings.",
         link: "https://bassamstracker.netlify.app/"
     },
     {
         title: "Calculator",
-        src: "calc",
+        src: "calc.png",
         codedin: ["https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png", "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"],
         description: "This project was written over the course of one afternoon because I was practicing my skills. It turned out much better than I was expecting. I haven't taught it the order of operations yet though.",
         link: "https://bassamscalculator.netlify.app/"
     },
     {
         title: "Meme Generator",
-        src: "meme",
+        src: "meme.png",
         codedin: ["https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png", "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"],
         description: "This project was made using React.js. It was one of my introductory projects to the React library, and helped me get accustomed to the quirkiness of forms in React.",
         link: "https://basmemegen.netlify.app/"
     },
     {
         title: "Goku Drawing (unfinished)",
-        src: "goku",
+        src: "goku.png",
         codedin: ["https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png"],
         description: "This is an unfinished project that I've been working on using Python's tkinter library. It's a drawing of one of my personal favourite characters, Goku.",
         link: "https://github.com/BassGI1/GokuDrawing"
     },
     {
         title: "Minesweeper",
-        src: "mine",
+        src: "mine.png",
         codedin: ["https://i.ibb.co/ZN6L4KK/C-Icon.png", "https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png"],
         description: "This is a project that was done for one of my courses. It is a fully functional minesweeper game that was done using C and C++. I did not write the entire project, rather I wrote one file, and the rest were provided by the instructional staff of ECE 150.",
         link: "https://github.com/BassGI1/Minesweeper"
     },
     {
         title: "Website For a Pizza",
-        src: "pizza",
+        src: "pizza.png",
         codedin: ["https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"],
         description: "This is the first web project I ever made. It's a silly pizza designer website using vanilla JavaScript, CSS, and HTML.",
         link: "https://website-for-a-pizza.netlify.app/"
