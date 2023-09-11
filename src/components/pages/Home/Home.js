f3bc55326cf0281f042c20ba8d6356e6e0932783
Bassam <basmaym148@gmail.com>
2023-09-11 19:37:25 -0400

    finished cover video

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -10,7 +10,7 @@ export default function Home(props) {
             
             <h1 style={{color: colour, fontSize: "3.25rem"}} className="welcome">Welcome to my personal website!</h1>
 
-            <iframe width="600" height="350" src="https://www.youtube.com/embed/Zv5WHkgHyMk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen className="video"></iframe>
+            <iframe width="600" height="350" src="https://www.youtube.com/embed/nOZd_hVjq3c?si=JDpHeyQrXqX81lE_" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen className="video"></iframe>
 
             <p className="routetovids" style={{color: colour}} onClick={() => props.setPage('Vids')}>Click here to view all co-op videos</p>
 
--- src/components/pages/Home/Videos.js
+++ src/components/pages/Home/Videos.js
@@ -5,7 +5,7 @@ export default function Videos(props) {
 
     //add terms to the start of the videos array
 
-    let videos = [{term: "Summer 2023", src: "https://www.youtube.com/embed/Zv5WHkgHyMk"}, {term: "Fall 2022", src: "https://www.youtube.com/embed/ewkrrM4R33A"}, {term: 'Winter 2022', src: "https://www.youtube.com/embed/ExHA_JmflF0"}]
+    let videos = [{term: "Winter 2024", src: "https://www.youtube.com/embed/nOZd_hVjq3c?si=JDpHeyQrXqX81lE_"}, {term: "Summer 2023", src: "https://www.youtube.com/embed/Zv5WHkgHyMk"}, {term: "Fall 2022", src: "https://www.youtube.com/embed/ewkrrM4R33A"}, {term: 'Winter 2022', src: "https://www.youtube.com/embed/ExHA_JmflF0"}]
 
     return (
 
