c2a67a7aa2a642271949298e37c4d096a8d70e6e
Bassam <basmaym148@gmail.com>
2024-11-06 13:26:37 -0500

    idk

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -1,3 +1,9 @@
+import "./AboutMe.css"
+
 export default function AboutMe() {
-	return <div className="main-wrapper"></div>
+	return <div className="main-wrapper">
+		<div className="card about-me-top-card">
+
+		</div>
+	</div>
 }
